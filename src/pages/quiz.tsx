2adc81c67f1901d0c25726f663f9909a7478b718
manuelsanchez2 <manusansan22@gmail.com>
2023-08-15 10:59:07 +0200

    Add pagination in the quiz 🎾

--- src/pages/quiz.tsx
+++ src/pages/quiz.tsx
@@ -1,11 +1,12 @@
 import Layout from '@/components/layout/Layout';
 import Seo from '@/components/Seo';
 import Hero from '@/components/hero/Hero';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { supabase } from '@/lib/api';
 import { load } from 'cheerio';
 import { jellyFishInfo, urlJellyFishMotril } from '@/constant/constants';
 import { type JellyFishProps } from '@/types/types';
+import usePagination from '@/hooks/usePagination';
 
 export async function getServerSideProps(): Promise<{ props: JellyFishProps }> {
   try {
@@ -50,11 +51,30 @@ export default function QuizPage(props: JellyFishProps) {
     fetchNames();
   }, [data]);
 
+  const initialState = {
+    currentPage: 1,
+    pageSize: 10,
+    total: data.length,
+  };
+  const [state, actions] = usePagination(initialState);
+
+  const isPrevDisabled = state.currentPage === 1;
+  const isNextDisabled =
+    state.currentPage === Math.ceil(data.length / state.pageSize) ||
+    data.length === 0;
+
   async function fetchNames() {
     let { data: names, error } = await supabase.from('users').select('*');
     if (error) console.log('error', error);
     else setData(names);
   }
+
+  const displayedPeople = useMemo(() => {
+    const sortedData = data.sort((a, b) => b.score - a.score);
+    const start = (state.currentPage - 1) * state.pageSize;
+    const end = state.currentPage * state.pageSize;
+    return sortedData.slice(start, end);
+  }, [data, state.currentPage, state.pageSize]);
 
   return (
     <Layout marqueeData={props}>
@@ -83,32 +103,49 @@ export default function QuizPage(props: JellyFishProps) {
                 </tr>
               </thead>
               <tbody>
-                {data
-                  .sort((a, b) => b.score - a.score)
-                  .map((item) => {
-                    const medal = () => {
-                      if (item.score >= 10) {
-                        return '🥇';
-                      } else if (item.score >= 8) {
-                        return '🥈';
-                      } else if (item.score >= 7) {
-                        return '🥉';
-                      } else {
-                        return '';
-                      }
-                    };
-                    return (
-                      <tr key={item.id}>
-                        <td>
-                          {medal()}
-                          {item.name}
-                        </td>
-                        <td>{item.score}</td>
-                      </tr>
-                    );
-                  })}
+                {displayedPeople.map((item) => {
+                  const medal = () => {
+                    if (item.score >= 10) {
+                      return '🥇';
+                    } else if (item.score >= 8) {
+                      return '🥈';
+                    } else if (item.score >= 7) {
+                      return '🥉';
+                    } else {
+                      return '';
+                    }
+                  };
+                  return (
+                    <tr key={item.id}>
+                      <td className='min-w-[150px]'>
+                        {medal()}
+                        {item.name}
+                      </td>
+                      <td>{item.score}</td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
+
+            <div className='flex flex-col mt-12'>
+              <div className='flex justify-center gap-8'>
+                <button
+                  onClick={actions.prevPage}
+                  disabled={isPrevDisabled}
+                  className='disabled:text-gray-300 disabled:pointer-events-none'
+                >
+                  Anteriores
+                </button>
+                <button
+                  onClick={actions.nextPage}
+                  disabled={isNextDisabled}
+                  className='disabled:text-gray-300 disabled:pointer-events-none'
+                >
+                  Siguientes
+                </button>
+              </div>
+            </div>
           </div>
         </section>
       </main>
