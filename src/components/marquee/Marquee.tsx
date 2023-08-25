18d649f91dc5dcc9769d9f1d206c501251ead787
manuelsanchez2 <manusansan22@gmail.com>
2023-08-25 17:53:17 +0200

    Fix issue

--- src/components/marquee/Marquee.tsx
+++ src/components/marquee/Marquee.tsx
@@ -5,8 +5,8 @@ import Marquee from 'react-fast-marquee';
 
 export default function FastMarquee({ data }: { data: any }) {
   const jellyFishMessage = processJellyfishInfo(data.jellyFishAmount);
-  const [_, state] = data.waveInfoPower.trim().split(/\s+/);
-  const waterTemp = data.tempWaterInfo.trim();
+  const [_, state] = data.waveInfoPower?.trim().split(/\s+/) || [];
+  const waterTemp = data.tempWaterInfo?.trim() || [];
 
   return (
     <Marquee className='h-16' speed={100}>
@@ -17,15 +17,15 @@ export default function FastMarquee({ data }: { data: any }) {
           {jellyFishMessage.translation}
         </span>
       </div>
-      {state.length > 0 && <div className='mx-5'>+++</div>}
-      {state.length > 0 && (
+      {state?.length > 0 && <div className='mx-5'>+++</div>}
+      {state?.length > 0 && (
         <div className='mx-5'>
           Nivel de oleaje: <span className={`ml-3 font-bold`}>{state}</span>
         </div>
       )}
 
-      {waterTemp.length > 0 && <div className='mx-5'>+++</div>}
-      {waterTemp.length > 0 && (
+      {waterTemp?.length > 0 && <div className='mx-5'>+++</div>}
+      {waterTemp?.length > 0 && (
         <div className='mx-5'>
           <span className={`ml-3 font-bold text-[var(--color-primary-700)]`}>
             {waterTemp}
